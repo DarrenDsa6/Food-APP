1d8b27de10f39891d383932833ac091bd79101c7
davy23jones <dkshetty23032023@gmail.com>
2023-05-26 11:05:59 +0530

    Dashboard ui

---
+++ app/src/main/java/com/example/food_ordering_app/Adapter/FoodListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.food_ordering_app.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.food_ordering_app.Activity.FoodDomain;
+
+import java.util.ArrayList;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.ViewHolder> {
+    ArrayList<FoodDomain> items;
+    Context context;
+
+    public FoodListAdapter(ArrayList<FoodDomain> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_food_list,parent,false);
+        context = parent.getContext();
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
